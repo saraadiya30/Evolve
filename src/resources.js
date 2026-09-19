7b2f20b5fa6a2be6639b298a3ea355e8b1805c5f
Alfaro Johnatan Herawan <alfaro@gpay.id>
2026-09-19 08:20:56 +0700

    fix

--- src/resources.js
+++ src/resources.js
@@ -3107,59 +3107,59 @@ export function initAether(){
     let wrap = $(`<div id="aetherExchange"></div>`);
     $('#aether').append(wrap);
 
-    wrap.append($(`<h3 class="has-text-info" style="margin:0 0 .5rem 0;">${loc('resource_Aether_name')}: <span>{{ p.Aether.count | round }}</span></h3>`));
+    wrap.append($(`<h3 class="has-text-info" style="margin:0 0 .5rem 1rem;">${loc('resource_Aether_name')}: <span>{{ p.Aether.count | round }}</span></h3>`));
 
-    let genRow = $(`<div id="aetherGen" class="market-item"><h3 class="res has-text-info">${loc('aether_gen_label')}</h3></div>`);
+    let genRow = $(`<div id="aetherGen" class="market-item" style="margin-bottom:.5rem;"><h3 class="res has-text-info">${loc('aether_gen_label')}</h3></div>`);
     wrap.append(genRow);
     genRow.append($(`<b-tooltip label="${loc('aether_gen_custom')}" position="is-bottom" size="is-small" multilined animated><label class="checkbox" style="margin-right:.5rem;white-space:nowrap;"><input type="checkbox" v-model="s.aetherCustomRateOn"> ${loc('aether_gen_custom_short')}</label></b-tooltip>`));
     genRow.append($(`<input type="number" min="0" step="1" v-model.number="s.aetherCustomRate" style="width:6rem;margin-right:.5rem;background:#1a1a1a;color:inherit;border:1px solid #555;">`));
     genRow.append($(`<span class="current infoOnly">{{ genLabel() }}</span>`));
 
-    let plasmidRow = $(`<div id="aetherPlasmid" class="market-item"><h3 class="res has-text-info">${loc('resource_Plasmid_name')}</h3></div>`);
+    let plasmidRow = $(`<div id="aetherPlasmid" class="market-item" style="margin-bottom:.5rem;"><h3 class="res has-text-info">${loc('resource_Plasmid_name')}</h3></div>`);
     wrap.append(plasmidRow);
     plasmidRow.append($(`<span class="buy"><span class="has-text-success">${loc('resource_market_buy')}</span></span>`));
     plasmidRow.append($(`<b-tooltip label="${loc('aether_exchange_button',[1,loc('resource_Aether_name'),1000,loc('resource_Plasmid_name')])}" position="is-bottom" size="is-small" multilined animated><span role="button" class="order" :class="{ off: p.Aether.count < 1 }" @click="buyPlasmid()">1000</span></b-tooltip>`));
     plasmidRow.append($(`<span class="trade"><span class="has-text-warning">${loc('aether_exchange_route')}</span><b-tooltip :label="stepInfo(1)" position="is-bottom" size="is-small" multilined animated><span role="button" aria-label="decrease plasmid rate" class="sub has-text-danger" @click="lessPlasmidRate"><span>-</span></span></b-tooltip><span class="current">{{ s.aetherPlasmidRate }}</span><b-tooltip :label="stepInfo(1)" position="is-bottom" size="is-small" multilined animated><span role="button" aria-label="increase plasmid rate" class="add has-text-success" @click="morePlasmidRate"><span>+</span></span></b-tooltip></span>`));
 
-    let phageRow = $(`<div id="aetherPhage" class="market-item"><h3 class="res has-text-info">${loc('resource_Phage_name')}</h3></div>`);
+    let phageRow = $(`<div id="aetherPhage" class="market-item" style="margin-bottom:.5rem;"><h3 class="res has-text-info">${loc('resource_Phage_name')}</h3></div>`);
     wrap.append(phageRow);
     phageRow.append($(`<span class="buy"><span class="has-text-success">${loc('resource_market_buy')}</span></span>`));
     phageRow.append($(`<b-tooltip label="${loc('aether_exchange_button',[1,loc('resource_Aether_name'),100,loc('resource_Phage_name')])}" position="is-bottom" size="is-small" multilined animated><span role="button" class="order" :class="{ off: p.Aether.count < 1 }" @click="buyPhage()">100</span></b-tooltip>`));
     phageRow.append($(`<span class="trade"><span class="has-text-warning">${loc('aether_exchange_route')}</span><b-tooltip :label="stepInfo(1)" position="is-bottom" size="is-small" multilined animated><span role="button" aria-label="decrease phage rate" class="sub has-text-danger" @click="lessPhageRate"><span>-</span></span></b-tooltip><span class="current">{{ s.aetherPhageRate }}</span><b-tooltip :label="stepInfo(1)" position="is-bottom" size="is-small" multilined animated><span role="button" aria-label="increase phage rate" class="add has-text-success" @click="morePhageRate"><span>+</span></span></b-tooltip></span>`));
 
-    let moneyBuyRow = $(`<div id="aetherMoneyBuy" class="market-item"><h3 class="res has-text-info">${loc('resource_Money_name')}</h3></div>`);
+    let moneyBuyRow = $(`<div id="aetherMoneyBuy" class="market-item" style="margin-bottom:.5rem;"><h3 class="res has-text-info">${loc('resource_Money_name')}</h3></div>`);
     wrap.append(moneyBuyRow);
     moneyBuyRow.append($(`<input type="number" step="any" v-model.number="s.aetherMoneyQtyCoef" style="width:3.5rem;margin-right:.25rem;background:#1a1a1a;color:inherit;border:1px solid #555;">`));
     moneyBuyRow.append($(`<select v-model.number="s.aetherMoneyQtyExp" style="background:#1a1a1a;color:inherit;border:1px solid #555;margin-right:.5rem;">${aetherMoneyUnitOptions()}</select>`));
-    moneyBuyRow.append($(`<b-tooltip :label="moneyLabel()" position="is-bottom" size="is-small" multilined animated><span role="button" class="order" :class="{ off: p.Aether.count < moneyQty() }" @click="buyMoney()">${loc('resource_market_buy')}</span></b-tooltip>`));
+    moneyBuyRow.append($(`<b-tooltip :label="moneyLabel()" position="is-bottom" size="is-small" multilined animated><span role="button" class="order has-text-success" :class="{ off: p.Aether.count < moneyQty() }" @click="buyMoney()">${loc('resource_market_buy')}</span></b-tooltip>`));
 
-    let moneyRateRow = $(`<div id="aetherMoneyRate" class="market-item"><h3 class="res has-text-info">${loc('aether_exchange_route')}</h3></div>`);
+    let moneyRateRow = $(`<div id="aetherMoneyRate" class="market-item" style="margin-bottom:.5rem;"><h3 class="res has-text-info">${loc('aether_exchange_route')}</h3></div>`);
     wrap.append(moneyRateRow);
     moneyRateRow.append($(`<input type="number" step="any" v-model.number="s.aetherMoneyRateCoef" style="width:3.5rem;margin-right:.25rem;background:#1a1a1a;color:inherit;border:1px solid #555;">`));
     moneyRateRow.append($(`<select v-model.number="s.aetherMoneyRateExp" style="background:#1a1a1a;color:inherit;border:1px solid #555;margin-right:.5rem;">${aetherMoneyUnitOptions()}</select>`));
     moneyRateRow.append($(`<span class="current infoOnly">{{ moneyRateLabel() }}</span>`));
 
-    let powerRow = $(`<div id="aetherPower" class="market-item"><h3 class="res has-text-info">${loc('aether_power_label')}</h3></div>`);
+    let powerRow = $(`<div id="aetherPower" class="market-item" style="margin-bottom:.5rem;"><h3 class="res has-text-info">${loc('aether_power_label')}</h3></div>`);
     wrap.append(powerRow);
     powerRow.append($(`<span class="trade" style="margin-left:0"><span class="has-text-warning">${loc('aether_exchange_route')}</span><b-tooltip :label="stepInfo(0.00001)" position="is-bottom" size="is-small" multilined animated><span role="button" aria-label="decrease power draw" class="sub has-text-danger" @click="lessPower"><span>-</span></span></b-tooltip><span class="current">{{ s.aetherPower | round }}</span><b-tooltip :label="stepInfo(0.00001)" position="is-bottom" size="is-small" multilined animated><span role="button" aria-label="increase power draw" class="add has-text-success" @click="morePower"><span>+</span></span></b-tooltip></span>`));
     powerRow.append($(`<b-tooltip :label="'aether_exchange_power' | label([s.aetherPower * 100000000])" position="is-bottom" size="is-small" multilined animated><span class="order">${loc('aether_info_label')}</span></b-tooltip>`));
 
-    let storageRow = $(`<div id="aetherStorage" class="market-item"><h3 class="res has-text-info">${loc('tab_storage')}</h3></div>`);
+    let storageRow = $(`<div id="aetherStorage" class="market-item" style="margin-bottom:.5rem;"><h3 class="res has-text-info">${loc('tab_storage')}</h3></div>`);
     wrap.append(storageRow);
     storageRow.append($(`<span class="trade" style="margin-left:0"><span class="has-text-warning">${loc('aether_exchange_route')}</span><b-tooltip :label="stepInfo(0.00000000001)" position="is-bottom" size="is-small" multilined animated><span role="button" aria-label="decrease storage draw" class="sub has-text-danger" @click="lessStorage"><span>-</span></span></b-tooltip><span class="current">{{ s.aetherStorage | round }}</span><b-tooltip :label="stepInfo(0.00000000001)" position="is-bottom" size="is-small" multilined animated><span role="button" aria-label="increase storage draw" class="add has-text-success" @click="moreStorage"><span>+</span></span></b-tooltip></span>`));
     storageRow.append($(`<b-tooltip :label="'aether_exchange_storage' | label([(s.aetherStorage / 0.00000000001) * 100])" position="is-bottom" size="is-small" multilined animated><span class="order">${loc('aether_info_label')}</span></b-tooltip>`));
 
-    let speedRow = $(`<div id="aetherSpeed" class="market-item"><h3 class="res has-text-info">${loc('aether_speed_label')}</h3></div>`);
+    let speedRow = $(`<div id="aetherSpeed" class="market-item" style="margin-bottom:.5rem;"><h3 class="res has-text-info">${loc('aether_speed_label')}</h3></div>`);
     wrap.append(speedRow);
     speedRow.append($(`<span class="trade" style="margin-left:0"><span class="has-text-warning">${loc('aether_exchange_route')}</span><b-tooltip :label="stepInfo(1)" position="is-bottom" size="is-small" multilined animated><span role="button" aria-label="decrease speed draw" class="sub has-text-danger" @click="lessSpeed"><span>-</span></span></b-tooltip><span class="current">{{ s.aetherSpeed }}</span><b-tooltip :label="stepInfo(1)" position="is-bottom" size="is-small" multilined animated><span role="button" aria-label="increase speed draw" class="add has-text-success" @click="moreSpeed"><span>+</span></span></b-tooltip></span>`));
     speedRow.append($(`<b-tooltip :label="'aether_exchange_speed' | label([s.aetherSpeed + 1])" position="is-bottom" size="is-small" multilined animated><span class="order">${loc('aether_info_label')}</span></b-tooltip>`));
 
-    let moraleRow = $(`<div id="aetherMorale" class="market-item"><h3 class="res has-text-info">${loc('aether_morale_label')}</h3></div>`);
+    let moraleRow = $(`<div id="aetherMorale" class="market-item" style="margin-bottom:.5rem;"><h3 class="res has-text-info">${loc('aether_morale_label')}</h3></div>`);
     wrap.append(moraleRow);
     moraleRow.append($(`<span class="trade" style="margin-left:0"><span class="has-text-warning">${loc('aether_exchange_route')}</span><b-tooltip :label="stepInfo(0.00000000001)" position="is-bottom" size="is-small" multilined animated><span role="button" aria-label="decrease morale draw" class="sub has-text-danger" @click="lessMorale"><span>-</span></span></b-tooltip><span class="current">{{ s.aetherMorale | round }}</span><b-tooltip :label="stepInfo(0.00000000001)" position="is-bottom" size="is-small" multilined animated><span role="button" aria-label="increase morale draw" class="add has-text-success" @click="moreMorale"><span>+</span></span></b-tooltip></span>`));
     moraleRow.append($(`<b-tooltip :label="'aether_exchange_morale' | label([(s.aetherMorale / 0.00000000001) * 100])" position="is-bottom" size="is-small" multilined animated><span class="order">${loc('aether_info_label')}</span></b-tooltip>`));
 
-    let prodRow = $(`<div id="aetherProd" class="market-item"><h3 class="res has-text-info">${loc('aether_prod_label')}</h3></div>`);
+    let prodRow = $(`<div id="aetherProd" class="market-item" style="margin-bottom:.5rem;"><h3 class="res has-text-info">${loc('aether_prod_label')}</h3></div>`);
     wrap.append(prodRow);
     prodRow.append($(`<span class="trade" style="margin-left:0"><span class="has-text-warning">${loc('aether_exchange_route')}</span><b-tooltip :label="stepInfo(0.000000001)" position="is-bottom" size="is-small" multilined animated><span role="button" aria-label="decrease production draw" class="sub has-text-danger" @click="lessProd"><span>-</span></span></b-tooltip><span class="current">{{ s.aetherProd | round }}</span><b-tooltip :label="stepInfo(0.000000001)" position="is-bottom" size="is-small" multilined animated><span role="button" aria-label="increase production draw" class="add has-text-success" @click="moreProd"><span>+</span></span></b-tooltip></span>`));
     prodRow.append($(`<b-tooltip :label="'aether_exchange_prod' | label([(s.aetherProd / 0.000000001) * 100])" position="is-bottom" size="is-small" multilined animated><span class="order">${loc('aether_info_label')}</span></b-tooltip>`));
